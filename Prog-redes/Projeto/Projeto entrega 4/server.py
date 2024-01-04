4135353b7645b2580cb1e42478f9fce2d2692a1e
Victor Mateus da Silva <137106185+21nho@users.noreply.github.com>
2024-01-03 23:19:10 -0300

    Update server.py
    Foi implementado junto ao comando de threads dos clientes-servidor o comando do bot do telegram para que os dois comandos funcionem simultaneamente.

--- Prog-redes/Projeto/Projeto entrega 4/server.py
+++ Prog-redes/Projeto/Projeto entrega 4/server.py
@@ -1,24 +1,27 @@
-import threading
-import socket
+import threading, socket, requests
+from funcoes import *
 SERVER = '0.0.0.0'
 PORT = 5678
 clients = []
 
 def main():
+    telegram_thread = threading.Thread(target=telegrambot)
+    telegram_thread.start()
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
         server.bind((SERVER, PORT))
         server.listen(5)
-        print ("Recebendo conexões em : ", (SERVER, PORT))
+        print("Recebendo conexões em: ", (SERVER, PORT))
     except:
         return print('\nNão foi possível iniciar o servidor!\n')
 
     while True:
         client, addr = server.accept()
-        print (f"\nConexão de: {addr}\n")
+        print(f"\nConexão de: {addr}\n")
         clients.append(client)
 
+        # Inicia uma thread para interação com o cliente
         thread = threading.Thread(target=cliInteraction, args=[client, addr])
         thread.start()
 
@@ -34,7 +37,7 @@ def cliInteraction(client, addr):
 
 def broadcast(msg, client, addr):
     msg = f"{addr}: {msg.decode('utf-8')}"
-    print (msg)
+    print(msg)
     for clientItem in clients:
         if clientItem != client:
             try:
@@ -44,5 +47,28 @@ def broadcast(msg, client, addr):
 
 def deleteClient(client):
     clients.remove(client)
+
+def telegrambot():
+    token = "6731238648:AAF0nfYufZRsi9MgkGor1AcCoaxol8jJ9tk"
+    strURL = f"https://api.telegram.org/bot{token}/"
+    update_id = None
+    arquivo_registros = "registros.json"
+    registros = carregar_registros(arquivo_registros)
+
+    while True:
+        try:
+            atualizacao = pegar_atualizacao(update_id, strURL)
+            mensagens = atualizacao["result"]
+            for mensagem in mensagens:
+                update_id = mensagem['update_id']
+                chat_id = mensagem['message']['from']['id']
+                try:comando = mensagem['message']['text']
+                except:comando = "..."
+                resposta = criar_resposta(comando, chat_id, registros)
+                link_de_envio = f"{strURL}sendMessage?chat_id={chat_id}&text={resposta}"
+                requests.get(link_de_envio)
+        except OSError:
+            print("Ocorreu um erro... ", OSError)
+        salvar_registros(registros, arquivo_registros)
 
 main()
